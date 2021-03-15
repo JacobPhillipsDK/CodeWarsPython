291970d67a8c4ad08a703ba9d578d43d9ae2cfe8
Jacob Phillips <me@jacobphillips.dk>
2021-03-15 13:26:10 +0100

    Initial commit

---
+++ 6kyu_Format_string_names_like_bart.py
@@ -0,0 +1,30 @@
+"""
+Link : https://www.codewars.com/kata/53368a47e38700bd8300030d/train/python
+
+                                -----------------------------------------------
+                                                Description
+                                -----------------------------------------------
+
+Given: an array containing hashes of names Return: a string formatted as a list of names separated by commas except
+for the last two names, which should be separated by an ampersand. Example:
+
+namelist([ {'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'} ])
+# returns 'Bart, Lisa & Maggie'
+
+namelist([ {'name': 'Bart'}, {'name': 'Lisa'} ])
+# returns 'Bart & Lisa'
+
+namelist([ {'name': 'Bart'} ])
+# returns 'Bart'
+
+namelist([])
+# returns ''
+
+"""
+
+
+def namelist(names):
+    #your code here
+    return None
+
+
